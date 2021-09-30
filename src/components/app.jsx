e0ebcc80cb244b5cbe35c3f4be1ff0da32d348fa
Brian Gershon <briangershon@users.noreply.github.com>
2021-09-29 23:42:15 -0700

    Initial commit

---
+++ src/components/app.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function App({ message }) {
+  return (
+    <>
+      <h1>Hello</h1>
+      <main id="main" className="content">{message}</main>
+    </>
+  );
+}
+
+App.propTypes = {
+  message: PropTypes.string,
+};
+
+export default App;
